47fbc14d48317d8ebefc136bdf6678de39946f74
Utley <utley2600@gmail.com>
2015-09-19 00:33:24 -0400

    added parse function to graph - still quite glitchy

--- lang.js
+++ lang.js
@@ -1,9 +1,10 @@
 var parse = function( str ){
   var terms = str.split(/[\+,\-]/);
-  return terms;
-
-
-
+  for(var i in terms){
+    if(terms[i].indexOf('x') > -1){
+      return new Function('tmp','return '+terms[i].charAt(terms[i].indexOf('x')-1)+'*arguments[0];');
+    }
+  }
 };
 
 
