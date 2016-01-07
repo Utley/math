db318e2bcbd1090b433023f0cdac4c504e74c760
Utley <utley2600@gmail.com>
2016-01-07 12:32:46 -0500

    reworked parser

--- lang.js
+++ lang.js
@@ -1,14 +1,64 @@
-var parse = function( str ){
-  var terms = str.split(/[\+,\-]/);
-  var functionString = 'return '+terms[0].substr(0,terms[0].indexOf('x'))+'*arguments[0]';
-  for(var i in terms){
-    if(terms[i].indexOf('x') > -1){
-      return new Function('tmp',functionString);
+var parse = function( expressionString ){
+  var terms = [];
+  var tmp = new term();
+  var str = '';
+  var negative = false;
+  console.log('hi');
+  for(var i = 0; i < expressionString.length; i++){
+    var curr = expressionString.charAt(i);
+    if(curr == '+'){
+        tmp.coefficient = Number(str);
+        if(negative){
+          tmp.coefficient *= -1;
+        }
+        terms.push(tmp.clone());
+        str = '';
+        tmp = new term();
+        negative = false;
     }
+    else if(curr == '-'){
+      tmp.coefficient = Number(str);
+      terms.push(tmp.clone());
+      tmp = new term();
+      str = '';
+      str += curr;
+      tmp.coefficient = Number(str);
+      negative = true;
+    }
+    else if(curr.match(/[a-zA-Z]/)){
+      tmp.variable = curr;
+    }
+    else{
+        str += curr;
+    }
+    if( i == expressionString.length-1 ){
+      tmp.coefficient = Number(str);
+      terms.push(tmp.clone());
+      tmp = new term();
+      str = '';
+    }
+
+  }
+
+  if(terms.length > 1){
+    return terms;
+  }
+  else{
+    return tmp;
   }
 };
 
-var func = new function(){
-
-
+var term = function(){
+  this.coefficient = 1;
+  this.exponent = 1;
+  this.variable = 1;
+  this.divisor = 1;
+  this.clone = function(){
+    var tmp = new term();
+    tmp.coefficient = this.coefficient;
+    tmp.exponent = this.exponent;
+    tmp.variable = this.variable;
+    tmp.divisor = this.divisor;
+    return tmp;
+  }
 };
