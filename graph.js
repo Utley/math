f3054f884faa36f247ec74d84e01dad732d0789d
utley <utley2600@gmail.com>
2016-01-19 12:54:04 -0500

    added grid lines to graph

--- graph.js
+++ graph.js
@@ -4,14 +4,15 @@ canvas.width = window.innerWidth-50;
 canvas.height= window.innerHeight-50; //account for default padding/margin
 
 var makeGraph = function(expr){
+  drawGrid();
   var offsetX = canvas.width/2;
   var offsetY = canvas.height/2;
   ctx.lineWidth = 1;
-  var step = .25;
+  var step = .05;
   var min = -10;
   var max = 10;
   var range = max - min;
-  var scale = window.innerWidth / range;
+  var scale = canvas.width / range;
   var variables = {
     'x': min
   };
@@ -27,5 +28,30 @@ var makeGraph = function(expr){
     ctx.stroke();
     ctx.beginPath();
     ctx.moveTo(canvasX,canvasY);
+  }
+};
+var drawGrid = function(){
+  var dist = 1;
+  var width = canvas.width;
+  var height = canvas.height;
+  var min = -10;
+  var max = 10;
+  var range = max - min;
+  var scaleX = width / range;
+  var scaleY = height / 10;
+  //for every integer x value
+  for(var i = 0; i < range; i++){
+    ctx.beginPath();
+    ctx.moveTo( scaleX * i, 0);
+    ctx.lineTo( scaleX * i, height );
+    ctx.strokeStyle = "grey";
+    ctx.stroke();
+  }
+  for(var i = 0; i < range; i++){
+    ctx.beginPath();
+    ctx.moveTo( 0, scaleY * i);
+    ctx.lineTo( width, scaleY * i );
+    ctx.strokeStyle = "grey";
+    ctx.stroke();
   }
 };
