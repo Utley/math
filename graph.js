45513e93d4af9a50e913f28153f5d66b54c70ad3
utley <utley2600@gmail.com>
2016-01-20 16:34:48 -0500

    added array for graphing multiple lines

--- graph.js
+++ graph.js
@@ -14,9 +14,11 @@ var graph = function( mcanvas ){
   this.step = .05;
   this.min = -10;
   this.max = 10;
+  this.plots = ['x^2'];
   this.range = this.max - this.min;
   this.scaleX = canvas.width / this.range;
   this.scaleY = canvas.width / this.range; //technically wrong but it makes the graph look ok
+
   mcanvas.addEventListener("mousedown", function(){
     this.dragging = true;
   });
@@ -36,27 +38,35 @@ var graph = function( mcanvas ){
       g.render('x^2');
     }
   });
-  this.render = function(expr){
+  this.addPlot = function( expr ){
+    this.plots.push( expr );
+  };
+  this.render = function(){
     this.clear();
     this.drawGrid();
-    ctx.lineWidth = 1;
-    var variables = {
-      'x': this.min
-    };
-    var initX = this.scaleX * this.min + this.offsetX;
-    var initY = mcanvas.height - this.scaleY * evalFunction(expr, variables) - this.offsetY;
-    ctx.moveTo( initX, initY );
-    for(var i = this.min; i <= this.max; i += this.step){
-      ctx.strokeStyle="black";
-      ctx.lineWidth = "2";
-      variables.x = i;
-      var y = evalFunction( expr, variables );
-      var canvasX = this.scaleX * i + this.offsetX;
-      var canvasY = canvas.height - this.scaleY*y - this.offsetY;
-      ctx.lineTo(canvasX, canvasY);
-      ctx.stroke();
-      ctx.beginPath();
-      ctx.moveTo(canvasX,canvasY);
+    for(var i = 0; i < this.plots.length; i++){
+      ctx.lineWidth = 1;
+      var variables = {
+        'x': this.min
+      };
+      var initX = this.scaleX * this.min + this.offsetX;
+      if(!this.plots[i]){
+        return;
+      }
+      var initY = mcanvas.height - this.scaleY * evalFunction(this.plots[i], variables) - this.offsetY;
+      ctx.moveTo( initX, initY );
+      for(var j = this.min; j <= this.max; j += this.step){
+        ctx.strokeStyle="black";
+        ctx.lineWidth = "2";
+        variables.x = j;
+        var y = evalFunction( this.plots[i], variables );
+        var canvasX = this.scaleX * j + this.offsetX;
+        var canvasY = canvas.height - this.scaleY * y - this.offsetY;
+        ctx.lineTo( canvasX, canvasY );
+        ctx.stroke();
+        ctx.beginPath();
+        ctx.moveTo( canvasX, canvasY );
+      }
     }
   };
   this.drawGrid = function(){
@@ -86,5 +96,5 @@ var graph = function( mcanvas ){
   this.clear = function(){
     ctx.clearRect(0,0,this.width,this.height);
     this.drawGrid();
-  }
+  };
 };
