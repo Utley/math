f4681c846cd2b32ea44c5f5b82ef5f875c5b9891
utley <utley2600@gmail.com>
2016-01-21 10:53:33 -0500

    cleaned up code + added comments

--- graph.js
+++ graph.js
@@ -2,22 +2,20 @@ var canvas = document.getElementById("graph");
 var ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth-50;
 canvas.height= window.innerHeight-50; //account for default padding/margin
-canvas.addEventListener('mousemove',function(event){
 
-});
 var graph = function( mcanvas ){
   this.offsetX = mcanvas.width / 2;
-  this.offsetY = mcanvas.height / 2;
-  this.dragging = false;
+  this.offsetY = mcanvas.height / 2; //center of canvas
   this.width = mcanvas.width;
   this.height = mcanvas.height;
-  this.step = .05;
-  this.min = -10;
-  this.max = 10;
+  this.step = .05; //interval for evaluating function
+  this.min = -10;  //minimum display value
+  this.max = 10;   //maxmimum display value
   this.plots = ['x^2'];
   this.range = this.max - this.min;
-  this.scaleX = canvas.width / this.range;
+  this.scaleX = canvas.width / this.range; //ratio of pixels to mathematical units
   this.scaleY = canvas.width / this.range; //technically wrong but it makes the graph look ok
+  this.dragging = false; //for event listeners
 
   mcanvas.addEventListener("mousedown", function(){
     this.dragging = true;
@@ -29,18 +27,27 @@ var graph = function( mcanvas ){
     event.preventDefault();
     g.offsetX -= event.deltaX;
     g.offsetY += event.deltaY;
-    g.render('x^2'); //default
+    g.render(); //default
   });
   mcanvas.addEventListener("mousemove", function(event){
     if( this.dragging ){
       g.offsetX += event.movementX;
       g.offsetY -= event.movementY;
-      g.render('x^2');
+      g.render();
     }
   });
+
+  //used to java, sorry
   this.addPlot = function( expr ){
     this.plots.push( expr );
   };
+  this.getPlots = function(){
+    return this.plots;
+  };
+  this.removePlot = function( index ){
+    this.plots.splice( index, 1 );
+  };
+
   this.render = function(){
     this.clear();
     this.drawGrid();
@@ -49,10 +56,10 @@ var graph = function( mcanvas ){
       var variables = {
         'x': this.min
       };
-      var initX = this.scaleX * this.min + this.offsetX;
       if(!this.plots[i]){
         return;
       }
+      var initX = this.scaleX * this.min + this.offsetX;
       var initY = mcanvas.height - this.scaleY * evalFunction(this.plots[i], variables) - this.offsetY;
       ctx.moveTo( initX, initY );
       for(var j = this.min; j <= this.max; j += this.step){
@@ -60,8 +67,8 @@ var graph = function( mcanvas ){
         ctx.lineWidth = "2";
         variables.x = j;
         var y = evalFunction( this.plots[i], variables );
-        var canvasX = this.scaleX * j + this.offsetX;
-        var canvasY = canvas.height - this.scaleY * y - this.offsetY;
+        var canvasX =                  this.scaleX * j + this.offsetX;
+        var canvasY = canvas.height - (this.scaleY * y + this.offsetY);
         ctx.lineTo( canvasX, canvasY );
         ctx.stroke();
         ctx.beginPath();
@@ -69,6 +76,7 @@ var graph = function( mcanvas ){
       }
     }
   };
+  
   this.drawGrid = function(){
     ctx.lineWidth = 1;
     ctx.moveTo( 0, this.offsetY );
